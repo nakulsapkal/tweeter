4a8835e82fec257a70dea8bb37f0a07138a68da9
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-17 01:31:06 +0000

    adding tweets dynamically and added Jquery functions in client JS file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,71 +4,92 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
 
-const createTweetElement = function(obj) {
+  const createTweetElement = function(obj) {
 
-  const img = obj.user.avatars;
-  const tweetName = obj.user.name;
-  const tweetHandle = obj.user.handle;
-  const tweetText = obj.content.text;
-  const tweetDateTime = obj.created_at;
+    const img = obj.user.avatars;
+    const tweetName = obj.user.name;
+    const tweetHandle = obj.user.handle;
+    const tweetText = obj.content.text;
+    const tweetDateTime = obj.created_at;
 
-  const article = `<article class="tweet">
-	<div class="tweet-header">
-			<img src="${img}">
-			<div class="tweet-names">
-					<name>${tweetName}</name>
-					<handle>${tweetHandle}</handle>
-			</div>
-	</div>
-	<article class="tweet-text">${tweetText}</article>
-	<div class="tweet-details">
-			<output class="timeago">${tweetDateTime}</output>
-			<div class="tweet-icons">
-					<i class="fas fa-flag"></i>
-					<i class="fas fa-retweet"></i>
-					<i class="fas fa-heart"></i>
-			</div>
-	</div>
-</article>`;
+    const article = `<article class="tweet">
+		<div class="tweet-header">
+				<img src="${img}">
+				<div class="tweet-names">
+						<name>${tweetName}</name>
+						<handle>${tweetHandle}</handle>
+				</div>
+		</div>
+		<article class="tweet-text">${tweetText}</article>
+		<div class="tweet-details">
+				<output class="timeago">${tweetDateTime}</output>
+				<div class="tweet-icons">
+						<i class="fas fa-flag"></i>
+						<i class="fas fa-retweet"></i>
+						<i class="fas fa-heart"></i>
+				</div>
+		</div>
+	</article>`;
 
-  return article;
-}
-
-
-// Fake data taken from initial-tweets.json
-const data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1466663959088
+    return article;
   }
-]
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (const tweetObj of tweets) {
-    $('#tweets-container').append(createTweetElement(tweetObj)) // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweetObj of tweets) {
+      $('#tweets-container').append(createTweetElement(tweetObj)) // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
   }
-}
 
-//calling to append the tweets dynamically
-renderTweets(data);
+
+  const loadtweets = function() {
+    //get call of Jquery data for provided url.
+    $.get("http://localhost:8080/tweets", function(data) {
+      renderTweets(data);
+
+      $(".tweet").hover(function() {
+        $(this).addClass("tweet-shadow");
+      }, function() {
+        $(this).removeClass("tweet-shadow");
+        $(this).off(".tweet", function() {}, function() {});
+      })
+
+      $(".fa-flag").hover(function() {
+        $(this).addClass("tweet-icons-shadow");
+      }, function() {
+        $(this).removeClass("tweet-icons-shadow");
+        $(this).off("fa-flag");
+      });
+
+      $(".fa-retweet").hover(function() {
+        $(this).addClass("tweet-icons-shadow");
+      }, function() {
+        $(this).removeClass("tweet-icons-shadow");
+        $(this).off("fa-retweet");
+      });
+
+      $(".fa-heart").hover(function() {
+        $(this).addClass("tweet-icons-shadow");
+      }, function() {
+        $(this).removeClass("tweet-icons-shadow");
+        $(this).off("fa-heart");
+      });
+
+
+      $('.timeago').each(function() {
+        $(this).html(timeago.format($(this).html()));
+      });
+
+    });
+
+  }
+
+  //calling to load the tweets from URL
+  loadtweets();
+
+});
